3a4eea7169d7c2a33e74c8940e5ab0c122160b58
Yeeloman <yami.onlyme@gmail.com>
2024-04-01 16:09:21 +0000

    task1

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -1,1 +1,37 @@
 #!/usr/bin/env python3
+"""task 1"""
+
+import csv
+import math
+from typing import List
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+            assert isinstance(page, int) and isinstance(page_size, int)
+            assert page > 0 and page_size > 0
+            start, end = self.index_range(page, page_size)
+            return self.dataset()[start:end]
+
+    def index_range(self, page: int, page_size: int) -> tuple:
+        """returns a tuple of size two
+        containing a start index and an end index"""
+        return (page - 1) * page_size, page * page_size
--- 0x00-pagination/main.py
+++ 0x00-pagination/main.py
@@ -2,6 +2,11 @@
 """
 Main file
 """
+def task_printer(task: int) -> None:
+    """task printer"""
+    print(f"###################### Task {task} ######################")
+
+task_printer(0)
 
 index_range = __import__('0-simple_helper_function').index_range
 
@@ -12,3 +17,37 @@ print(res)
 res = index_range(page=3, page_size=15)
 print(type(res))
 print(res)
+
+task_printer(1)
+
+"""
+Main file
+"""
+
+Server = __import__('1-simple_pagination').Server
+
+server = Server()
+
+try:
+    should_err = server.get_page(-10, 2)
+except AssertionError:
+    print("AssertionError raised with negative values")
+
+try:
+    should_err = server.get_page(0, 0)
+except AssertionError:
+    print("AssertionError raised with 0")
+
+try:
+    should_err = server.get_page(2, 'Bob')
+except AssertionError:
+    print("AssertionError raised when page and/or page_size are not ints")
+
+
+print(server.get_page(1, 3))
+print(server.get_page(3, 2))
+print(server.get_page(3000, 100))
+
+task_printer(2)
+
+task_printer(3)
