6693ba611ab7ba5882d8dc52d668d9880c9db536
Yeeloman <yami.onlyme@gmail.com>
2024-04-09 15:36:02 +0000

    adding task 4

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -1,1 +1,39 @@
 #!/usr/bin/env python3
+"""3. Parametrize templates"""
+
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config:
+    """config class for flask"""
+    """initiate config class"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """determine the best
+    match with the
+    supported languages"""
+    local = request.args.get('locale')
+    if local in app.config['LANGUAGES']:
+        return local
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/',  strict_slashes=False)
+def index():
+    """serving the template"""
+    return render_template('3-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
